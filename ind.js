95785b360717547e4a67e85e05e78a96b4641360
swapnilhavale96 <swapnilhavale07@gmail.com>
2024-03-28 18:16:09 +0530

    ain

--- ind.js
+++ ind.js
@@ -12,7 +12,10 @@ let menu_btn = document.getElementsByClassName('bi-three-dots')[0];
 let menu_bx = document.getElementById('menu_bx');
 
 
-menu_btn.addEventListener('click', ()=>{
+menu_btn.addEventListener('click', ()=>{ 
+    let cn = menu_bx.classList;
     menu_bx.classList.toggle('ul_active');
 })
+
+
 
